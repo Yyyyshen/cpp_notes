10e6079f68ea0d3ffcd0121e044f429d6fface28
Yyyyshen <Yyyyshen@gmail.com>
2021-10-27 11:30:36 +0800

    学习极客专栏《罗剑锋C++实战笔记》

---
+++ LJF_cpp_notes_01/LJF_cpp_notes_01.cpp
@@ -0,0 +1,18 @@
+﻿// LJF_cpp_notes_01.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+//
+//极客专栏《罗剑锋C++实战笔记》 
+//
+
+//
+// 
+//
+
+int main()
+{
+	std::cout << "Hello World!\n";
+}
+
