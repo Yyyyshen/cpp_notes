920c5299a94580145e1edf80c2998b01f082a254
Yyyyshen <Yyyyshen@gmail.com>
2021-11-06 16:22:03 +0800

    技能进阶

---
+++ LJF_cpp_notes_05/LJF_cpp_notes_05.cpp
@@ -0,0 +1,21 @@
+﻿// LJF_cpp_notes_05.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+//技能进阶
+
+using namespace std;
+
+//
+//序列化
+//
+
+//
+//
+//
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
