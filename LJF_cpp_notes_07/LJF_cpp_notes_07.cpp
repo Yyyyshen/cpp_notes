ab663bc3180e8f5038bbd62ec21a59439cf3f63d
Yyyyshen <Yyyyshen@gmail.com>
2021-11-09 11:13:18 +0800

    应用实践

---
+++ LJF_cpp_notes_07/LJF_cpp_notes_07.cpp
@@ -0,0 +1,21 @@
+﻿// LJF_cpp_notes_07.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+//应用实践
+
+using namespace std;
+
+//
+//书店应用（C++ Primer中例子）
+//
+
+//
+//
+//
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
