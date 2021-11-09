b7c5e908ad04735a41dd5a260b09981e2e53d03c
Yyyyshen <Yyyyshen@gmail.com>
2021-11-09 09:44:29 +0800

    设计模式和应用

---
+++ LJF_cpp_notes_06/LJF_cpp_notes_06.cpp
@@ -0,0 +1,21 @@
+﻿// LJF_cpp_notes_06.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+//设计模式和应用
+
+using namespace std;
+
+//
+//设计模式
+//
+
+//
+//
+//
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
