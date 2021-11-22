ef64ecc3908b30b1bf2a11012a02d15de2d0ed70
Yyyyshen <Yyyyshen@gmail.com>
2021-11-22 11:27:18 +0800

    跳过STL

---
+++ cpp_standard_04/cpp_standard_04.cpp
@@ -0,0 +1,17 @@
+﻿// cpp_standard_04.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+//
+//STL
+//
+
+//
+//中间STL章节跟其他的书内容差不多，篇幅很大跳过了
+//
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
