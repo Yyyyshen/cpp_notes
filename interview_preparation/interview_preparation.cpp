4306045f0413a9d336590976b336eec52fd4998c
Yyyyshen <Yyyyshen@gmail.com>
2021-12-28 16:33:41 +0800

    笔记

---
+++ interview_preparation/interview_preparation.cpp
@@ -0,0 +1,17 @@
+﻿// interview_preparation.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+//
+//准备面试中比较杂的内容
+//
+
+//
+//
+//
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
