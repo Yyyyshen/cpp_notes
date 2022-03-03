bff7e1bdaab0ac854bcd6b7c6ae86ca622c794f4
Yyyyshen <Yyyyshen@gmail.com>
2022-03-03 12:04:11 +0800

    准备

--- interview_preparation/interview_preparation.cpp
+++ interview_preparation/interview_preparation.cpp
@@ -8,7 +8,27 @@
 //
 
 //
+//项目经验
+// SDK
+// WEB
+// 驱动
+// Win
+// Linux
+// 
+//准备一些原理
+// boost asio
+// beast
+// wfp
+// minifilter
+// IO复用
 //
+
+//
+//针对安恒windows相关的
+// 看过书，理论阶段，没有实践
+// 《Windows黑客编程技术详解》 
+// 《Windows内核编程》
+// 对驱动，底层api都有了解
 //
 
 int main()
